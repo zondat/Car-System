36332bccecca0d966510ebc8342adeb5806a5760
zondat <datnguyen101191@gmail.com>
2025-08-31 01:56:46 +0700

    Integrate Camera and headtracker

--- controller/Car_v3.ino
+++ controller/Car_v3.ino
@@ -2,8 +2,9 @@
 /******* Car setting **********/
 #include "car.hpp"
 
+
 Car car;
-Servo servo1, servo2;
+Servo servo1, servo2, servo3;
 
 /********************************/
 /******* Radio setting **********/
@@ -26,20 +27,34 @@ void init_radio() {
 void handle_cmd(RadioPacket _mess) {
   if (_mess.upGear) car.upGearLevel();
   if (_mess.downGear) car.downGearLevel();
-  
   if (_mess.brake) car.brake();
-  else {
-    int pwm;
+    
+  // Accelerate
+  handle_accel(_mess.x);
+  // Turn
+  handle_turn(_mess.y);
+  // Headtrack
+  handle_head_track(_mess.yaw);
+}
 
-    // Accelerate
-    if (_mess.x<ADC_MID) _mess.x = ADC_MID;
-    pwm = map(_mess.x, ADC_MIN, ADC_MAX, PWM_MIN, PWM_MAX);
-    car.accelerate(pwm);
+void handle_accel(uint16_t accel) {
+  car.accelerate(accel);
+}
 
-    // Turn
-    pwm = map(_mess.y, ADC_MIN, ADC_MAX, PWM_MIN, PWM_MAX);
-    car.turn(pwm);
-  }
+void handle_turn(uint16_t yaw) {
+  int pwm = map(yaw, ADC_MIN, ADC_MAX, PWM_MIN, PWM_MAX);
+  car.turn(pwm);
+}
+
+/********************************/
+/******* Headtracking  **********/
+void init_headtrack() {
+  servo3.attach(PIN_SERVO_CAM);
+  servo3.write(90);
+}
+
+void handle_head_track(uint8_t yaw) {
+  servo3.write(yaw);
 }
 
 /********************************/
@@ -53,10 +68,11 @@ void setup() {
   car.esc = servo1;
   car.yawServo = servo2;
   car.init();
-  car.turn(1500);
 
   // Init RF module
   init_radio();
+
+  init_headtrack();
 }
 
 void loop() {
@@ -68,14 +84,17 @@ void loop() {
       handle_cmd(_radioData);
       nbMisses = 0;
     } else {
-      nbMisses ++;
-      if (nbMisses<NB_MAX_MISSED) handle_cmd(_lastData);
-      else {
-        car.brake();
-        nbMisses = NB_MAX_MISSED;
-      }
-      break;
+      car.brake();
+      handle_turn(_lastData.y);
+      handle_head_track(_lastData.yaw);
+      // nbMisses ++;
+      // if (nbMisses<NB_MAX_MISSED) handle_cmd(_lastData);
+      // else {
+      //   car.brake();
+      //   nbMisses = NB_MAX_MISSED;
+      // }
+      // break;
     } 
   }
-  delay(45);
+  delay(100);
 }
